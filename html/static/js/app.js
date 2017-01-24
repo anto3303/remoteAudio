02707b498c8d212d2b9580d80e6e2aed18a41302
dh1tw <tobias@dh1tw.de>
2017-01-24 03:54:01 +0100

    added chart and bootstrap theme; improved WebUI

--- html/static/js/app.js
+++ html/static/js/app.js
@@ -1,3 +1,7 @@
+$.material.init();
+
+
+
 new Vue({
     el: '#app',
 
@@ -19,7 +23,15 @@ new Vue({
             var msg = JSON.parse(e.data);
             self.connectionStatus = msg.connectionStatus;
             self.txUser = msg.txUser;
-            self.ping = msg.ping;
+            if (latencyChart.data.datasets[0].data.length >= 20) {
+                latencyChart.data.datasets[0].data.shift();
+            }
+            if (msg.ping > 500){
+                latencyChart.data.datasets[0].data.push(500); // truncate
+            } else {
+                latencyChart.data.datasets[0].data.push(msg.ping);
+            }
+            latencyChart.update(0.1);
             self.tx = msg.tx;
             self.serverAudioOn = msg.serverAudioOn;
             self.serverOnline = msg.serverOnline;
@@ -48,14 +60,90 @@ new Vue({
         },
         sendPtt: function () {
             // if (this.serverAudioOn) {
-                this.ws.send(
-                    JSON.stringify({
-                        ptt: !this.tx,
-                    }));
+            this.ws.send(
+                JSON.stringify({
+                    ptt: !this.tx,
+                }));
             // }
         },
     }
-})
+});
+
+var volumeSlider = document.getElementById('volumeSlider');
+
+noUiSlider.create(volumeSlider, {
+    start: [50],
+    connect: [true, false],
+    // tooltips: [ true ],
+    range: {
+        'min': 0,
+        'max': 100
+    },
+    pips: { // Show a scale with the slider
+        mode: 'steps',
+        stepped: true,
+        density: 5
+    }
+});
+
+var data = {
+    labels: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    datasets: [{
+        label: "Latency",
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65],
+        spanGaps: false,
+    }]
+};
+
+var ctx = document.getElementById("latencyChart");
+var latencyChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: {
+        legend: {
+            display: false,
+        },
+        // animation:{
+        //     duration: 2000,
+        //     animation: 'easeInOutQuad',
+        // },
+        responsive: true,
+        layout: {
+            padding: {
+                left: 10,
+                right: 20,
+                top: 20
+            },
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 500,
+                    min: 0,
+                    stepSize: 50
+                }
+            }],
+        }
+    }
+});
+
 
 // socket.onopen = function (event) {
 //     console.log("Socket opened successfully");
