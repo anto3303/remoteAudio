10f6c8c676b39761ef09195729771b37cef711e2
dh1tw <tobias@dh1tw.de>
2017-01-28 13:15:55 +0100

    corrected url

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -2,7 +2,7 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  wget https://https://github.com/google/protobuf/archive/v3.1.0.tar.gz
+  wget https://github.com/google/protobuf/archive/v3.1.0.tar.gz
   tar -xzvf v3.1.0.tar.gz
   cd protobuf-3.1.0 && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make install
 else
