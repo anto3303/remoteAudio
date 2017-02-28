71ae7442dae50728e8b552fa0cce0f9a4127b0c2
dh1tw <tobias@dh1tw.de>
2017-02-28 23:50:51 +0100

    cache

--- ci/install-deps-windows.bat
+++ ci/install-deps-windows.bat
@@ -6,7 +6,9 @@ if "%METHOD%"=="ci" SET MSYS_PATH=c:\msys64
 if "%METHOD%"=="cross" SET MSYS_PATH=%APPVEYOR_BUILD_FOLDER%\msys%MSYS2_BITS%
 SET PATH=%MSYS_PATH%\usr\bin;%PATH%
 SET PATH=%MSYS_PATH%\mingw%MSYS2_BITS%\bin;%PATH%
+if Not Exist %MSYS_PATH%\msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz ( 
 if "%METHOD%"=="cross" appveyor DownloadFile http://kent.dl.sourceforge.net/project/msys2/Base/%MSYS2_ARCH%/msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz
+)
 if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz > nul
 if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar > nul
 %MSYS_PATH%\usr\bin\bash -lc "echo update-core starting..." 2> nul
