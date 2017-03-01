30e717089f8e8b5b1a5bffa70d44ea8b70084ba5
dh1tw <tobias@dh1tw.de>
2017-03-01 01:55:15 +0100

    win

--- ci/install-deps-windows.bat
+++ ci/install-deps-windows.bat
@@ -26,5 +26,3 @@ if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar > nul
 %MSYS_PATH%\usr\bin\bash -lc "pacman --noconfirm --needed -Sy curl" > nul
 %MSYS_PATH%\usr\bin\bash -lc "yes|pacman --noconfirm -Sc" > nul
 if "%METHOD%"=="cross" %MSYS_PATH%\autorebase.bat > nul
-%MSYS_PATH%\usr\bin\bash -lc "echo $GOOS"
-%MSYS_PATH%\usr\bin\bash -lc "echo $GOARCH"
