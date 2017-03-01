c4c310c4090905093331dee9dc6d984696ac2d17
dh1tw <tobias@dh1tw.de>
2017-03-01 02:00:26 +0100

    rrr

--- ci/build-windows-bundle.bat
+++ ci/build-windows-bundle.bat
@@ -1,14 +1,12 @@
-mkdir c:\release\
-%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libogg-0.dll /c/release/"
-%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libopus-0.dll /c/release/"
-%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libopusfile-0.dll /c/release/"
-%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libportaudio-2.dll /c/release/"
-%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libsamplerate-0.dll /c/release/"
+mkdir %GOPATH%\src\github.com\dh1tw\remoteAudio\release\
+%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libogg-0.dll /c/gopath/src/github.com/dh1tw/remoteAudio/release/"
+%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libopus-0.dll /c/gopath/src/github.com/dh1tw/remoteAudio/release/"
+%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libopusfile-0.dll /c/gopath/src/github.com/dh1tw/remoteAudio/release/"
+%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libportaudio-2.dll /c/gopath/src/github.com/dh1tw/remoteAudio/release/"
+%MSYS_PATH%\usr\bin\bash -lc "cp /mingw%MSYS2_BITS%/**/libsamplerate-0.dll /c/gopath/src/github.com/dh1tw/remoteAudio/release/"
 REM %MSYS_PATH%\usr\bin\bash -lc "pacman --noconfirm --needed -Sy sed" > nul
 REM %MSYS_PATH%\usr\bin\bash -lc "cd /c/gopath/src/github.com/dh1tw/remoteAudio && ci/release"
-%MSYS_PATH%\usr\bin\bash -lc "cd /c/gopath/src/github.com/dh1tw/remoteAudio && ls -al"
-REM %MSYS_PATH%\usr\bin\bash -lc "cp /c/gopath/src/github.com/dh1tw/remoteAudio/remoteAudio.exe /c/release/"
-REM %MSYS_PATH%\usr\bin\bash -lc "cd /c/gopath/src/github.com/dh1tw/remoteAudio/release && 7z a -tzip remoteAudio.zip *"
-REM %MSYS_PATH%\usr\bin\bash -lc "cd /c/gopath/src/github.com/dh1tw/remoteAudio/release && rm *.dll"
-REM %MSYS_PATH%\usr\bin\bash -lc "cd /c/gopath/src/github.com/dh1tw/remoteAudio/release && rm *.exe"
-REM xcopy %GOPATH%\src\github.com\dh1tw\remoteAudio\release\remoteAudio.zip %APPVEYOR_BUILD_FOLDER%\ /e /i > nul
+%MSYS_PATH%\usr\bin\bash -lc "cp /c/gopath/src/github.com/dh1tw/remoteAudio/remoteAudio.exe /c/gopath/src/github.com/dh1tw/remoteAudio/release"
+%MSYS_PATH%\usr\bin\bash -lc "cd /c/gopath/src/github.com/dh1tw/remoteAudio/release && 7z a -tzip remoteAudio.zip *"
+REM %MSYS_PATH%\usr\bin\bash -lc "cd /c/gopath/src/github.com/dh1tw/remoteAudio && rm -rf ./release"
+xcopy %GOPATH%\src\github.com\dh1tw\remoteAudio\release\remoteAudio.zip %APPVEYOR_BUILD_FOLDER%\ /e /i > nul
