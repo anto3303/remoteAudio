a8c3ee7d17e214ed69d359732343f738e813b810
dh1tw <tobias@dh1tw.de>
2017-03-03 00:40:31 +0100

    protoc

--- ci/install-deps-linux-osx.sh
+++ ci/install-deps-linux-osx.sh
@@ -7,8 +7,8 @@ env GOOS=$GIMME_OS GOARCH=$GIMME_ARCH go get -d ./...
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install pkg-config opus opusfile portaudio protobuf libsamplerate
 else #Linux
+    # Ubuntu 16.04 comes with an old version of protobuf. 
+    # We have to download and install a newer one
     ./ci/install-protobuf.sh
     export PATH=$PATH:$HOME/protobuf/bin
 fi
-
-protoc --proto_path=./icd --gofast_out=./sb_audio ./icd/audio.proto
