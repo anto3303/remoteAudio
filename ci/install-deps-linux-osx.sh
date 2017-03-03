7938f67001f0109f4009cd5a52d0ec246b78df54
dh1tw <tobias@dh1tw.de>
2017-03-03 01:13:25 +0100

    d

--- ci/install-deps-linux-osx.sh
+++ ci/install-deps-linux-osx.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-env GOOS=$GIMME_OS GOARCH=$GIMME_ARCH go get github.com/gogo/protobuf/protoc-gen-gofast
-env GOOS=$GIMME_OS GOARCH=$GIMME_ARCH go get github.com/GeertJohan/go.rice/rice
-env GOOS=$GIMME_OS GOARCH=$GIMME_ARCH go get -d ./...
+if not [[$GIMME_ARCH == 'amd']]; then
+    env GOOS=$GIMME_OS GOARCH=$GIMME_ARCH go get github.com/gogo/protobuf/protoc-gen-gofast
+    env GOOS=$GIMME_OS GOARCH=$GIMME_ARCH go get github.com/GeertJohan/go.rice/rice
+    env GOOS=$GIMME_OS GOARCH=$GIMME_ARCH go get -d ./...
+fi
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
@@ -15,6 +17,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 else #Linux
     # Ubuntu 16.04 comes with an old version of protobuf. 
     # We have to download and install a newer one
-    ./ci/install-protobuf.sh
-    export PATH=$PATH:$HOME/protobuf/bin
+    if not [[$GIMME_ARCH == 'amd']]; then
+        ./ci/install-protobuf.sh
+        export PATH=$PATH:$HOME/protobuf/bin
+    else 
+        ./ci/cross-compile-linux-arm.sh
+    fi
 fi
