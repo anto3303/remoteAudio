c03d4dd599a49133261530ccaca996f08ab026ff
dh1tw <tobias@dh1tw.de>
2017-03-03 01:05:39 +0100

    travis

--- ci/install-deps-linux-osx.sh
+++ ci/install-deps-linux-osx.sh
@@ -5,7 +5,13 @@ env GOOS=$GIMME_OS GOARCH=$GIMME_ARCH go get github.com/GeertJohan/go.rice/rice
 env GOOS=$GIMME_OS GOARCH=$GIMME_ARCH go get -d ./...
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    brew install pkg-config opus opusfile portaudio protobuf libsamplerate
+    brew update
+    brew outdated pkg-config || brew upgrade pkg-config
+    brew outdated opus || brew upgrade opus
+    brew outdated opusfile || brew upgrade opusfile
+    brew outdated portaudio || brew upgrade portaudio
+    brew outdated protobuf || brew upgrade protobuf
+    brew outdated libsamplerate || brew upgrade libsamplerate
 else #Linux
     # Ubuntu 16.04 comes with an old version of protobuf. 
     # We have to download and install a newer one
